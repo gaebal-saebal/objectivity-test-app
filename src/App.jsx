893aa131f945ffcc56654b45434fcc2ee8bb7961
js-ha <jshaha0911@gmail.com>
2023-05-17 17:07:26 +0900

    feat : 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Start } from './components/Start';
+import { Test } from './components/Test';
 
 // 시작페이지, 문항 컴포넌트 나와야 되는 페이지
 function App() {
   return (
     <>
       <Header />
-      <Start questionNumber={'001'} />
+      <Start />
+      <Test />
     </>
   );
 }
