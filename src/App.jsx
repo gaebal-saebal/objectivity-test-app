080789409a4d71b92f3cb8febeb48374e27b46cf
js-ha <jshaha0911@gmail.com>
2023-05-18 18:05:46 +0900

    feat : 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Start } from './components/Start';
 import { Test } from './components/Test';
 import Result from './pages/Result';
 import { question001 } from '../constants';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // 시작페이지, 문항 컴포넌트 나와야 되는 페이지
 function App() {
@@ -43,46 +44,57 @@ function App() {
   }, [correctArr]);
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <Start step={step} onClick={handleStepPlus} />
+              <Test
+                page={1}
+                step={step}
+                correctArr={correctArr}
+                setState={setCorrectArr}
+                handleStepPlus={handleStepPlus}
+                handleStepMinus={handleStepMinus}
+              />
+              <Test
+                page={2}
+                step={step}
+                correctArr={correctArr}
+                setState={setCorrectArr}
+                handleStepPlus={handleStepPlus}
+                handleStepMinus={handleStepMinus}
+              />
+              <Test
+                page={3}
+                step={step}
+                correctArr={correctArr}
+                setState={setCorrectArr}
+                handleStepPlus={handleStepPlus}
+                handleStepMinus={handleStepMinus}
+              />
+              <Test
+                page={4}
+                step={step}
+                correctArr={correctArr}
+                setState={setCorrectArr}
+                handleStepPlus={handleStepPlus}
+                handleStepMinus={handleStepMinus}
+                handleSubmit={handleSubmit}
+              />
+            </>
+          }
+        ></Route>
+        <Route path='/result' element={<Result />}></Route>
+      </Routes>
       <div>정답지 : {correctArr}</div>
       <div>맞춘 갯수 : {correctAmount}</div>
-      <Start step={step} onClick={handleStepPlus} />
-      <Test
-        page={1}
-        step={step}
-        correctArr={correctArr}
-        setState={setCorrectArr}
-        handleStepPlus={handleStepPlus}
-        handleStepMinus={handleStepMinus}
-      />
-      <Test
-        page={2}
-        step={step}
-        correctArr={correctArr}
-        setState={setCorrectArr}
-        handleStepPlus={handleStepPlus}
-        handleStepMinus={handleStepMinus}
-      />
-      <Test
-        page={3}
-        step={step}
-        correctArr={correctArr}
-        setState={setCorrectArr}
-        handleStepPlus={handleStepPlus}
-        handleStepMinus={handleStepMinus}
-      />
-      <Test
-        page={4}
-        step={step}
-        correctArr={correctArr}
-        setState={setCorrectArr}
-        handleStepPlus={handleStepPlus}
-        handleStepMinus={handleStepMinus}
-        handleSubmit={handleSubmit}
-      />
+
       {/* <Result /> */}
-    </>
+    </BrowserRouter>
   );
 }
 
