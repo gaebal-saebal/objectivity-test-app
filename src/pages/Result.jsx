5a33fb635b0e7e32eb59b409099e4ad4ed0527b4
js-ha <jshaha0911@gmail.com>
2023-05-19 12:42:59 +0900

    feat : 테스트 다시하기 버튼 기능 구현

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,8 +6,9 @@ import { useParams } from 'react-router-dom';
 //TODO : 총 점수내서 해당되는 결과 페이지로 보내주기 -> ok
 //TODO : 체크한 문항은 실시간으로 갯수 업데이트해주기
 //TODO : 체크 안해도 다음 페이지로 넘어가는거 막기
-//TODO : 버튼 공유하기 만들기(클립보드 복사 - 메인페이지)
+//TODO : 버튼 공유하기 만들기(클립보드 복사 - 메인페이지) -> ok
 //TODO : 결과페이지는 테스트 결과 공유하기로..! -> ok
+//TODO : 테스트다시하기 버튼 만들기 -> ok
 
 const Result = () => {
   let params = useParams();
@@ -47,7 +48,10 @@ const Result = () => {
         정보가 아무리 많아도 그중에 자신이 관심을 가지는 주관적인 정보만을 받아들이는것을 말합니다.
         객관성 수치가 높을수록 정보를 객관적으로 바라보는 능력이 뛰어납니다.
       </div>
-      <Button variant='outlined'>테스트 다시하기</Button>
+
+      <Button variant='outlined'>
+        <a href='/'>테스트 다시하기</a>
+      </Button>
       <Button variant='outlined' onClick={handleShare}>
         결과 공유하기
       </Button>
