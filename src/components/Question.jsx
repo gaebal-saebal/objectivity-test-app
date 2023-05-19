a438054d2d909b1efc3375c10f4fe34b5ca898bb
js-ha <jshaha0911@gmail.com>
2023-05-19 17:20:43 +0900

    design : 페이지 레이아웃 수정

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -14,23 +14,26 @@ export const Question = ({ correctArr, setCorrectArr, checkArr, setCheckArr, que
   const questionNumber = questionNo - 1;
   return (
     <div>
-      <div>
+      <div className='mb-5'>
         <span>{question001.questions[questionNumber].questionNo}.</span>
         {question001.questions[questionNumber].questionLabel}
       </div>
-      <div>
+      <div className='bg-pink-100 '>
         {question001.questions[questionNumber].options.map((option, i) => {
           return (
-            <div key={i}>
+            <div key={i} className='h-10 flex items-center '>
               <input
                 type='radio'
                 value={option.answer}
                 name={questionNumber}
                 id={`${questionNumber}-${i}`}
                 onClick={handleClick}
+                className='hidden'
               />
               {/* 텍스트 클릭해도 체크될 수 있도록 하기 위해서 */}
-              <label htmlFor={`${questionNumber}-${i}`}>{option.name}</label>
+              <label htmlFor={`${questionNumber}-${i}`} className='cursor-pointer '>
+                {option.name}
+              </label>
             </div>
           );
         })}
