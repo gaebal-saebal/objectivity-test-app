6a13165544a034e7c9d4f5c6fc2157f5ce79c9ec
rosenfence <rosenfence@gmail.com>
2023-05-22 10:47:52 +0900

    design : radio button style 구현

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -13,25 +13,30 @@ export const Question = ({ correctArr, setCorrectArr, checkArr, setCheckArr, que
 
   const questionNumber = questionNo - 1;
   return (
-    <div>
+    <div className='pb-5'>
       <div className='mb-5'>
-        <span>{question001.questions[questionNumber].questionNo}.</span>
+        <span>{`${question001.questions[questionNumber].questionNo}. `}</span>
         {question001.questions[questionNumber].questionLabel}
       </div>
-      <div className='bg-pink-100 '>
+      <div className='border rounded-lg'>
         {question001.questions[questionNumber].options.map((option, i) => {
           return (
-            <div key={i} className='h-10 flex items-center '>
+            <div key={i} className='relative flex items-center h-10 border-b last:border-b-0'>
               <input
                 type='radio'
                 value={option.answer}
                 name={questionNumber}
                 id={`${questionNumber}-${i}`}
                 onClick={handleClick}
-                className='hidden'
+                className={`w-[100%] h-10 px-3 appearance-none absolute hover:bg-orange-200 ${
+                  i === 0 ? 'rounded-t-lg' : i === 2 ? 'rounded-b-lg' : ''
+                } checked:bg-orange-500 transition`}
               />
               {/* 텍스트 클릭해도 체크될 수 있도록 하기 위해서 */}
-              <label htmlFor={`${questionNumber}-${i}`} className='cursor-pointer '>
+              <label
+                htmlFor={`${questionNumber}-${i}`}
+                className='flex items-center cursor-pointer w-[100%] h-[100%] z-10 px-3'
+              >
                 {option.name}
               </label>
             </div>
